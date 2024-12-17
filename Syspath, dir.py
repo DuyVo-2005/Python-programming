319ca2ef52d13997fe2c49b566fa2537336c04ec
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2024-12-17 22:49:25 +0700

    Create Syspath, dir.py

---
+++ Syspath, dir.py
@@ -0,0 +1,8 @@
+#Đường dẫn tìm kiếm module lưu trữ trong sys.path chứa thư mục hiện hành, PYTHONPATH, đường dẫn mặc định
+"""import sys
+
+print(sys.path)"""
+#Liệt kê thuộc tính, phương thức trong module sử dụng hàm dir()
+import math
+
+print(dir(math))
