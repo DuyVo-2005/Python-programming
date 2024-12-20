52237a5b205b5e94200f41efa75fb13a7b4a56e8
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2024-12-20 11:43:59 +0700

    Create String: basic syntax.py

---
+++ String: basic syntax.py
@@ -0,0 +1,13 @@
+#str1 = 'Hello'
+#str2 = "everyone!"
+#str3 =' \nWelcome to: '
+#str4 ='''\nPython class
+#Math class
+#... '''
+#nhieu dong phai nam trong 3 nhay don hoac kep
+#print(str1, str2, str3, str4)
+
+a = "hello World!"
+#print(a.lower()) # returns "hello world!"
+#print(a.upper()) # returns "HELLO WORLD!"
+print(a.capitalize()) # returns "Hello World!"
