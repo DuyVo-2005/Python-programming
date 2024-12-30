e349d57aa91c34be64b422a1916b812874c84f4c
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2024-12-30 13:29:11 +0700

    Create Empty window with title.py

---
+++ Tkinker library - GUI/Empty window with tittle.py
@@ -0,0 +1,19 @@
+#Thêm thư viện tkinter
+
+from tkinter import *
+
+#Tạo một cửa sổ mới
+
+window = Tk()
+
+#Thêm tiêu đề cho cửa sổ
+
+window.title('Welcome to HCMUTE app')
+#optional
+#Đặt kích thước của cửa sổ
+
+window.geometry('350x200')
+
+#Lặp vô tận để hiển thị cửa sổ
+
+window.mainloop()
